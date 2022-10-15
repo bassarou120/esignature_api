5ad740e14c68db4f2c4ef3f6154ca2cf2e7baa95
anagoarmandine <anagoarmandine@gmail.com>
2022-10-15 12:36:33 +0100

    update

--- app/Http/Controllers/API/SendingController.php
+++ app/Http/Controllers/API/SendingController.php
@@ -1166,7 +1166,7 @@ class SendingController extends BaseController
                             }
                             else{
                                 $tmp = public_path('/previews/tempimg.png');
-                                print_r($all_answer[$index]);
+                                print_r($all_answer[$index]->value);
                                 $dataURI    = $all_answer[$index]->value;
                                 $dataPieces = explode(',',$dataURI);
                                 $encodedImg = $dataPieces[1];
