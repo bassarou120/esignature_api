85251dced263bd8c18f7925aa91f41b4976fb499
anagoarmandine <anagoarmandine@gmail.com>
2023-01-27 15:57:26 +0100

    checking error

--- app/Http/Controllers/API/Authentification.php
+++ app/Http/Controllers/API/Authentification.php
@@ -63,56 +63,7 @@ class Authentification extends BaseController
     }
 
     public function Register(Request $request){
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string',
-            'email' => 'required|email|unique:users,email',
-            'phone_number' => 'nullable|unique:users,phone_number',
-            'entreprise' => 'nullable|string',
-            'password' => 'required|string|min:8|confirmed',
-            'member_id' => 'nullable|numeric',
 
-        ]);
-        if ($validator->fails()) {
-            return $this->sendError('Validation Error.', $validator->errors());
-        }
-        $input = $request->all();
-        echo 'validation';
-        $input['password'] = bcrypt($input['password']);
-        $user = User::create($input);
-        echo 'user created';
-
-        if(isset($request->member_id)){
-            if($request->member_id !=''){
-             $member=Member::find($request->member_id);
-             $member->membre=$user->id;
-             $member->save();
-            }
-            echo 'member created';
-        }
-
-        $ciphering = "AES-128-CTR";
-
-        $iv_length = openssl_cipher_iv_length($ciphering);
-        $options = 0;
-
-        $encryption_iv = '1234567891011121';
-
-        $encryption_key = "EsignDineCode";
-
-        $encryption = openssl_encrypt(
-            $request->email,
-            $ciphering,
-            $encryption_key,
-            $options,
-            $encryption_iv
-        );
-        $t = base64_encode($request->email);
-        echo 'b64 encoding';
-        Notification::route('mail', $user->email)
-            ->notify(new NewRegistration($t));
-
-        echo 'notify';
-        return $this->sendResponse(new UserResource($user), 'User register successfully.');
     }
 
     public function activate_account($token){
